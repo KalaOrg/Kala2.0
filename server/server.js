781c37616f1848a4a453aea2446f9dd5b71bbae8
Kayliegh.Hill <kayliegh.hill@gmail.com>
2022-02-18 17:31:16 -0500

    git add

---
+++ server/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const path = require('path');
+
+console.log(process.env);
+
+// initialize application
+const app = express();
+
+// set up CORS for Cross-Origin-Resource-Sharing
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'locahost:3000');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+// converts API responses to JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// serve static files
+app.use(express.static(path.resolve(__dirname, '../dist')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.get('/', (req, res) => {
+    console.log('hi');
+    res.sendFile(path.join(__dirname, './src/index.html'));
+  });
+}
+
+// catch-all route handler for any requests to an unknown route
+app.use('/*', (req, res) => res.sendStatus(404));
+
+// configire express global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(process.env.PORT, process.env.HOST, () =>
+  console.log(`Server running on ${process.env.HOST}:${process.env.PORT}`)
+);
+
+module.exports = app;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+});
+module.exports = {
+  entry: path.join(__dirname, 'src/main.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'build.js',
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+      publicPath: '/dist',
+    },
+    proxy: { '/api': 'http://localhost:3000' },
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+  },
+};
