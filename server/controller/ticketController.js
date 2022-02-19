2b60eca099a91984fe9e58c4b23fdfc9e14a61ab
Kayliegh.Hill <kayliegh.hill@gmail.com>
2022-02-19 11:42:01 -0500

    add new ticket route and middleware

--- server/controller/ticketController.js
+++ server/controller/ticketController.js
@@ -4,24 +4,59 @@ const path = require('path');
 const ticketController = {};
 
 ticketController.getTickets = (req, res, next) => {
-  fs.readFile(
+  fs.readFile(path.resolve(__dirname, '../data.json'), 'UTF-8', (err, data) => {
+    if (err) {
+      return next({
+        log: `ticketController.getTickets: ERROR: ${err}`,
+        message: {
+          err: 'ticketController.getTickets: ERROR: Check server logs for details',
+        },
+      });
+    }
+    const parsedData = JSON.parse(data);
+    res.locals.tickets = parsedData;
+    return next();
+  });
+};
+
+ticketController.addTicket = (req, res, next) => {
+  const {
+    first_name,
+    department,
+    issue_title,
+    issue_summary,
+    status,
+    priority,
+  } = req.body;
+  const date = new Date();
+  const _id = Math.floor(Math.random() * 1000);
+
+  res.locals.data = {
+    _id: _id,
+    first_name: first_name,
+    department: department,
+    issue_title: issue_title,
+    issue_summary: issue_summary,
+    status: status,
+    priority: priority,
+    time: date,
+  };
+
+  fs.appendFile(
     path.resolve(__dirname, '../data.json'),
+    JSON.stringify(res.locals.data, null, 2),
     'UTF-8',
-    (err, data) => {
+    (err) => {
       if (err) {
         return next({
-          log: `ticketController.getTickets: ERROR: ${err}`,
+          log: `ticketController.addTickets: ERROR: ${err}`,
           message: {
-            err: 'ticketController.getTickets: ERROR: Check server logs for details',
+            err: 'ticketController.addTickets: ERROR: Check server logs for details',
           },
         });
       }
-      const parsedData = JSON.parse(data);
-      res.locals.tickets = parsedData;
       return next();
     }
   );
-
-}
-
+};
 module.exports = ticketController;
