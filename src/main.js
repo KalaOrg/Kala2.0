b71f11b5ecb305ba57492cc1a2f10790f67f6f5c
Alison Fay <alisonfay@Nicholass-MacBook-Pro.local>
2022-02-19 12:08:05 -0500

    added react router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+//import '/index.css'
 
 const App = () => {
   return ( 
     <h1>Hello</h1>
   )
-  
 }
 
-ReactDOM.render(<App />, document.getElementyById('root'))
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementyById('root')
+);
