f4bf191a49c585e8157353fe21df7417803dd906
Alfonso Zamarripa <AlfonsoZamarripa@Alfonsos-MacBook-Pro.local>
2022-02-19 13:59:44 -0500

    create db connection

--- server/model/ticketModel.js
+++ server/model/ticketModel.js
@@ -14,7 +14,7 @@ const pool = new Pool({
 //   department_id     INTEGER REFERENCES department_table(_id),
 //   issue_title   VARCHAR(75),
 //   issue_summary  VARCHAR(500),
-//   status_id       INTEGER REFERENCES status_table(_id), <-- might work
+//   status _id      INTEGER REFERENCES status_table(_id)
 //   priority_id       INTEGER REFERENCES priority_table(_id),
 //   date           TIMESTAMPTZ
 // );
@@ -33,9 +33,11 @@ const pool = new Pool({
 //   _id       SERIAL PRIMARY KEY,
 //   name      VARCHAR(75)
 // );
+
 // We export an object that contains a property called query,
 // which is a function that returns the invocation of pool.query() after logging the query
 // This will be required in the controllers to be the access point to the database
+
 module.exports = {
   query: (text, params, callback) => {
     console.log('executed query', text);
