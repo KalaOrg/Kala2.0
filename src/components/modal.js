9dc80db0f5067208a1ba819552b56e5237a0306d
Linda Zhao <lindazhao115@gmail.com>
2022-02-20 16:57:23 -0500

    header complete, form in progress

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-const TicketForm = () => {
+const TicketForm = (props) => {
   //states
   const [enterName, setName] = useState('');
   const [department, setDepartment] = useState(''); //from dropdown
@@ -20,12 +20,12 @@ const TicketForm = () => {
       }
       const metaData = {
         method: 'POST',
-        headers: {'Content-Type: application/json'},
+        headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(body)
       }
       fetch('/add', metaData)
         .then(data => data.json())
-        .then(tickets => console.log(tickets))//not sure what to do with this after
+        .then(tickets => props.setTickets(tickets))//** may have to revise based on hook names **
     }
     catch(err){
       console.log(err.message);
@@ -33,35 +33,47 @@ const TicketForm = () => {
   }
 
   return (
-    <div>
+    <div className='container' id='form-container'>
       <h2>Ticket Entry Form</h2>
       <form onSubmit={submitTicket}>
-        <div>
-          <label htmlFor='enter-name'>Name</label>
-          <input type='text' value={enterName} onChange={(e => setName(e.target.value))}></input>
+        <div className="form-row">
+          <div className="form-group col-md-5">
+            <label htmlFor='enter-name'>Name</label>
+            <br></br>
+            <input type='text' className="form-control" required value={enterName} onChange={(e => setName(e.target.value))}></input>
+          </div>
+          <br></br>
+          <div className="form-group col-md-5">
+            <label htmlFor='enter-department'>Department</label>
+            <br></br>
+              <select className="form-control" required onChange={(e => setDepartment(e.target.value))}>
+                <option selected disabled value="">Choose a department</option>
+                <option value='1'>Software Engineering</option>
+                <option value='2'>Facilities</option>
+                <option value='3'>Marketing</option>
+              </select>
+          </div>
         </div>
-        <div>
-          <label htmlFor='enter-department'>Department</label>
-            <select onChange={(e => setDepartment(e.target.value))}>
-              <option value='1'>Software Engineering</option>
-              <option value='2'>Facilities</option>
-              <option value='3'>Marketing</option>
-            </select>
-        </div>
+        <br></br>
         <div>
           <h3>Ticket</h3>
           <label htmlFor='enter-ticket-title'>Title</label>
-          <input type='text' value={ticketTitle} onChange={(e => setTicketTitle(e.target.value))}></input>
+          <br></br>
+          <input type='text' className="form-control" required value={ticketTitle} onChange={(e => setTicketTitle(e.target.value))}></input>
           <br></br>
           <label htmlFor='enter-ticket-summary'>Summary</label>
-          <input type='text' value={ticketSummary} onChange={(e => setTicketSummary(e.target.value))}></input>
+          <br></br>
+          <textarea type='text' className="form-control" required value={ticketSummary} onChange={(e => setTicketSummary(e.target.value))}></textarea>
         </div>
+        <br></br>
         <div>
-          <label htmlFor='enter-priority'>Priority</label>
-            <select onChange={(e => setPriority(e.target.value))}>
-              <option value='1'>Software Engineering</option>
-              <option value='2'>Facilities</option>
-              <option value='3'>Marketing</option>
+          <label htmlFor='enter-priority'>Priority </label>
+          <br></br>
+            <select className='form-control' required onChange={(e => setPriority(e.target.value))}>
+              <option selected disabled value="">Choose a priority</option>
+              <option value='1'>Low</option>
+              <option value='2'>Medium</option>
+              <option value='3'>High</option>
             </select>
         </div>
       </form>
--- src/containers/headerContainer.js
+++ src/containers/headerContainer.js
@@ -6,7 +6,6 @@ const Header = (props) => {
     return (
       <div>
         <LogoHeader/>
-        <TicketEntryButton/>
       </div>
     )
 }
