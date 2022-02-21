0db9a52d9d6cfbbb463db495769993a06301d9dc
Alison Fay <alisonfay@Nicholass-MacBook-Pro.local>
2022-02-21 12:49:27 -0500

    progress with rendering tickets and ticket item logic

--- src/components/ticketColumns.js
+++ src/components/ticketColumns.js
@@ -1,11 +1,9 @@
-import { useEffect } from 'react';
-import { ProgressPlugin, ProvidePlugin } from 'webpack';
-import Ticket from '../components/ticket.js';
+import React, { useEffect, useState } from 'react';
+import Ticket from './tickets';
 
 const TicketColumn = (props) => {
-  
   const [tickets, setTickets] = useState([]);
-  
+
   // {
   //     _id: data._id,
   //     first_name: data.first_name,
@@ -16,31 +14,46 @@ const TicketColumn = (props) => {
   //     priority: data.priority,
   //     time: data.time,
   //   },
+  
   const ticketItems = [];
-  ticketItems.map((ticket, index) => (
-    <Ticket key={_id} index={index} ticket={ticket} />
-    ))}
-            
+  for (let i = 0; i < tickets.length; i++) {
+    console.log('TICKETS LENGTH:', tickets.length);
+    console.log('TICKETS:', tickets)
+    ticketItems.push(<Ticket key={i} ticket={tickets[i]} />);
+    console.log('TICKET ITEMS:', ticketItems)
+  }
+
+  // ticketItems.map((ticket, index) => (
+  //   <Ticket key={_id} index={index} ticket={ticket} />
+  //   ))
 
   const fetchTickets = () => {
     fetch('/api')
-    .then(response => response.json())
-    .then(data => setTickets(data.filter(() => {
-        return (props.priortity === data.priority);
-      })))
-  }
-  
+      .then((response) => response.json())
+      .then((data) =>
+        setTickets(
+          data.filter(() => {
+            return props.priortity === data.priority;
+          })
+        )
+      )
+      .catch((err) => console.log(err));
+  };
+
   useEffect(() => {
     fetchTickets();
-  })
+  }, []);
 
   return (
     <div>
       <div id={props.priority}>
+        {/* {console.log('tickets:', tickets)}
+        {console.log('ticket items:', ticketItems)} */}
         {ticketItems}
+        <p>hey there</p>
       </div>
     </div>
   );
-}
+};
 
 export default TicketColumn;
--- src/components/tickets.js
+++ src/components/tickets.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
+
 const Ticket = (props) => {
   //const { _id, first_name, department, issue_title, issue_summary, status, priority, date } = state.props
   
@@ -7,12 +8,28 @@ const Ticket = (props) => {
 
   })
 
-  return(
-    <div className='ticket-item'>
-
-      {[props.title]}
-    </div>
-  )
+  return (
+    <article>
+      <div className='ticketHead'>
+        <div>
+          <h3>{props.issue_title}</h3>
+          <h3>Issue Title Here</h3>
+        </div>
+        <button>X</button>
+      </div>
+      <div>
+        <ul className='ticketDetailList'>
+          {/* {console.log('props:', props)} */}
+          <li className='ticketDetail'>Summary: {props.issue_summary}</li>
+          <li className='ticketDetail'>Name: {props.first_name}</li>
+          <li className='ticketDetail'>Department: {props.department}</li>
+          <li className='ticketDetail'>Priority: {props.priority}</li>
+          <li className='ticketDetail'>Status: {props.status}</li>
+          <li className='ticketDetail'>Date: {props.date}</li>
+        </ul>
+      </div>
+    </article>
+  );
 };
 
 export default Ticket; 
